e1502bc5964055a8a40792bfe528c8d561ee760e
James Dietz <james-dietz@users.noreply.github.com>
2021-04-24 23:45:15 +0100

    feature(atlantis): generate embed containing Atlantis entrance info
    Creates a function generating the embed message sent in response to the request for Atlantis entrance information.

--- discord/atlantis.go
+++ discord/atlantis.go
@@ -1,7 +1,11 @@
 package discord
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/antihax/goesi"
+	"github.com/bwmarrin/discordgo"
 
 	"github.com/Convocation-of-Empyreans-Development/MALRO_incursion_bot/esi"
 )
@@ -17,4 +21,35 @@ func SetAtlantisEntranceLocation(client *goesi.APIClient, location string, confi
 		distances[system] = esi.GetDistanceToSystem(client, origin, destination)
 	}
 	config.AtlantisDistances = distances
+}
+
+// SendAtlantisLocationEmbed produces a message embed containing the Atlantis entrance information and
+// sends it in the channel where the command used to request it was issued.
+func SendAtlantisLocationEmbed(s *discordgo.Session, m *discordgo.MessageCreate, config Config) {
+	embed := &discordgo.MessageEmbed{
+		Type:  "",
+		Title: "Atlantis entrance",
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:   "Entrance system",
+				Value:  config.AtlantisEntrance,
+				Inline: true,
+			},
+			{
+				Name:   "Distance to home systems",
+				Value:  GenerateDistanceString(config.AtlantisDistances),
+				Inline: false,
+			},
+		},
+	}
+	s.ChannelMessageSendEmbed(m.ChannelID, embed)
+}
+
+// GenerateDistanceString creates a human-readable list of the distances to each of the home systems.
+func GenerateDistanceString(distances map[string]int) string {
+	builder := []string{}
+	for system, distance := range distances {
+		builder = append(builder, fmt.Sprintf("%s: %d", system, distance))
+	}
+	return strings.Join(builder, "\n")
 }
